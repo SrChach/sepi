fe0817131b7ebf470d44cd3a6e7a8320171300c8
srchach <gsikmo@gmail.com>
2019-03-02 17:22:06 -0600

    Vuex Getters. Obtain and give format to data

--- src/components/prueba.vue
+++ src/components/prueba.vue
@@ -1,7 +1,16 @@
 <template>
 	<div>
+		<h2>Acciones con Vuex</h2>
 		<button @click="estado">
-			Cambiar y imprimir estado vuex
+			modifica el estado
+		</button>
+		<br><br>
+		<button @click="obtener_simple">
+			Recibe datos de un getter simple
+		</button>
+		<br><br>
+		<button @click="getter_getter">
+			Getter que obtiene datos de otro getter
 		</button>
 	</div>
 </template>
@@ -17,7 +26,15 @@
 
 				// Acá imprimimos el valor 'count' del estado
 				console.log(`Propiedad 'count' incrementada desde un componente: ${this.$store.state.count}`)
+			},
+			obtener_simple(){
+				// Manda a llamar un getter de la $store
+				console.log(this.$store.getters.done_todos)
+			},
+			getter_getter(){
+				console.log(`Desde getter que obtiene datos de otro getter: ${this.$store.getters.done_todos_count}`)
 			}
+
 		}
 	}
 </script>
--- src/store.js
+++ src/store.js
@@ -4,15 +4,34 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+	// State contiene las propiedades sobre las que trabajaremos globalmente
 	state: {
-		count: 0
+		count: 0,
+		to_do: [
+			{ id: 1, text: 'Una tarea hecha', done: true },
+			{ id: 2, text: 'Una tarea fallida', done: false },
+			{ id: 2, text: 'Otra tarea hecha', done: true }
+		]
 	},
+	// Los getters formatean datos, para mostrarlos presentables
+	getters: {
+		done_todos: state => {
+			return state.to_do.filter(todo => todo.done)
+		},
+		done_todos_count: (state, getters) => {
+			return getters.done_todos.length
+		}
+	},
+	// Las mutations realizan cambios en los datos
 	mutations: {
 		increment(state) {
 			state.count++
 		}
 	},
 	actions: {
+
+	},
+	modules: {
 
 	}
 })
