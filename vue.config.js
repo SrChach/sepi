dc4078573f40dfbd68b169a924c545779daab99d
srchach <gsikmo@gmail.com>
2019-03-05 03:34:08 -0600

    Añadidos comentarios de borrar en vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
 module.exports = {
 	devServer: {
-		port: 8080, // Puerto que escucha de nuestra vue-app
+		port: 8080, // Puerto que escucha de nuestra vue-app (Aparentemente no necesario)
 		proxy: {
 			'/auth': {
 				target: 'http://127.0.0.1/api', // Sitio al que va a apuntar (redirigir)
 				pathRewrite: {'^/auth/': '/'}, // Para que, por ejemplo, no busque "auth" en el server de datos
-				secure: false,
+				secure: false, // (Aparentemente no necesario)
 				changeOrigin: true
 			},
 			'/api': { // sitio al que redirige
 				target: 'http://127.0.0.1/',
 				pathRewrite: {'^/api/': '/'},
-				secure: false
+				secure: false // (Aparentemente no necesario)
 			}
 		}
 	}
